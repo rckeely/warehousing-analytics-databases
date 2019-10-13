e3c6d708f49dba3adaa110d1b9d300a2d951b1c0
RC Keely <rckeely@gmail.com>
2019-10-13 14:17:04 +0200

    Added line to execute the requested queries and echo them to a file

--- homework/run_hw.sh
+++ homework/run_hw.sh
@@ -2,7 +2,7 @@
 
 # Load database using SQL file
 cat create_star_db.sql | docker run --net host -i postgres psql --host 0.0.0.0 --user postgres
-# Last week's creates company_db
-#cat create_company_db.sql | docker run --net host -i postgres psql --host 0.0.0.0 --user postgres
 # Perform ETL step
 python hw_w02.py
+# Execute the requested queries
+cat queries.sql | docker run --net host -i postgres psql --host 0.0.0.0 --user postgres > results.txt
