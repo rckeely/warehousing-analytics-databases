80b73cc043ca67158be79d82468baeea21809ee6
RC Keely <rckeely@gmail.com>
2019-10-14 22:37:16 +0200

    Updated script to cat results to console and provide more details

--- homework/run_hw.sh
+++ homework/run_hw.sh
@@ -5,4 +5,7 @@ cat create_star_db.sql | docker run --net host -i postgres psql --host 0.0.0.0 -
 # Perform ETL step
 python hw_w02.py
 # Execute the requested queries
+echo "Starting queries..."
 cat queries.sql | docker run --net host -i postgres psql --host 0.0.0.0 --user postgres > results.txt
+# Dump the results to the terminal
+cat results.txt
